f0dde00e4691b090a55d206907c195ed04521bfc
jenri019 <j.enrique209436@hotmail.com>
2024-02-11 12:35:34 -0600

    Adding input component to search

---
+++ src/common/components/inputSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const inputSearch = ({placeholder}) => {
+  return (
+    <>
+        <input
+            type="text"
+            placeholder='buscar'
+        />
+    </>
+  )
+}
