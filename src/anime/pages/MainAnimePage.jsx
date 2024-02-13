61d0a7f8ae54362a110e1d48d4ae7bd4825a62f1
jenri019 <j.enrique209436@hotmail.com>
2024-02-12 21:08:07 -0600

    Adding query string to components

--- src/anime/pages/MainAnimePage.jsx
+++ src/anime/pages/MainAnimePage.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import { AnimeHeader } from '../components'
 import { AnimeSearch } from './AnimeSearch'
+import queryString from 'query-string';
 import { useLocation, useParams } from 'react-router-dom';
 
 export const MainAnimePage = () => {
 
   const params = useParams();
   const location = useLocation();
-
-  console.log('PARAMS', params)
-  console.log('LOCATION', location)
+  const { anime = '' } = queryString.parse(location.search);
 
   return (
     <>
       <AnimeHeader />
-      <AnimeSearch />
+      <AnimeSearch search={anime}/>
     </>
   )
 }
