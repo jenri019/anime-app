84a2e121e75377a5c1b9d41e35cdb9c3ff11c9f5
jenri019 <j.enrique209436@hotmail.com>
2024-02-14 14:19:43 -0600

    Changing list page to a component

---
+++ src/anime/components/AnimeListContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AnimeHeader } from './AnimeHeader';
+import { AnimeList } from './AnimeList';
+
+export const AnimeListContainer = () => {
+
+  return (
+    <div>
+      <AnimeHeader />
+      <AnimeList />
+    </div>
+  )
+}
