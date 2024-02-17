1819f46a375968328ef803e1fcaa236a6dea50ae
jenri019 <j.enrique209436@hotmail.com>
2024-02-17 10:51:34 -0600

    Adding character by id page and showing info

--- src/characters/pages/CharacterIdPage.jsx
+++ src/characters/pages/CharacterIdPage.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useFetchData } from '../hooks';
 
 export const CharacterIdPage = () => {
+  const { pathname } = useLocation();
+  const {characters, isLoading} = useFetchData(pathname.substring(pathname.lastIndexOf('/') + 1), 'ID');
+
+  const navigate = useNavigate();
+
+  const onBackPage = () => {
+    navigate(-1);
+  }
+
   return (
-    <div>CharacterIdPage</div>
+    <>
+      <div className='row mt-5 animate__animated animate__fadeInRight'>
+        <div className="col-2">
+          <img src={characters?.images?.jpg?.image_url} alt={characters?.name} className='img-thumbnail'/>
+        </div>
+        <div className="col-6">
+          <h3>{characters?.name} <small>({characters?.name_kanji})</small></h3>
+          <h5 className='mt-3'>About</h5>
+          <p>{characters?.about}</p>
+          <hr />
+          <button className='btn btn-primary' onClick={() => onBackPage()}>Volver</button>
+        </div>
+      </div>
+    </>
   )
 }
