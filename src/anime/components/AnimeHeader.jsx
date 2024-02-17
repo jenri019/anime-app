b0612b0f3edefdf387bb0b1610a7293b3fcb585d
jenri019 <j.enrique209436@hotmail.com>
2024-02-17 10:16:13 -0600

    Removing code non used

--- src/anime/components/AnimeHeader.jsx
+++ src/anime/components/AnimeHeader.jsx
@@ -4,13 +4,6 @@ import { CommonContext } from '../../common/context/CommonContext'
 
 export const AnimeHeader = ({}) => {
 
-  const { setRoute } = useContext(CommonContext)
-
-  useEffect(() => {
-    setRoute('/anime/search')
-  }, []);
-  
-
   return (
     <>
         <SearchBar placeholder="Buscar anime por letra" limit={1}/>
