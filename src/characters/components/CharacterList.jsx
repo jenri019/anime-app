81eaded259264f85d96e8df47662f78221629a03
jenri019 <j.enrique209436@hotmail.com>
2024-02-17 10:50:00 -0600

    Adding content to character list

--- src/characters/components/CharacterList.jsx
+++ src/characters/components/CharacterList.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { CharacterCard } from './CharacterCard';
+import { useFetchData } from '../hooks';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 
 export const CharacterList = () => {
+  
+  const location = useLocation();
+  const { querySearch = '' } = queryString.parse(location.search);
+  const {characters = [], isLoading} = useFetchData (querySearch, 'FULL');
+
   return (
-    <div>CharacterList</div>
+    <>
+        <div className='character-list-grid animate__animated animate__fadeInRight mt-2'>
+        {
+          (isLoading)
+          ? (<h1>Cargando</h1> )
+          : (
+            characters.map( character => (
+                <CharacterCard key={character.mal_id} {...character} />
+            ))
+          )
+        }
+        </div>
+    </>
   )
 }
