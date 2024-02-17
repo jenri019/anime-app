6285f82ed2e730eef4a66b33d3d734c4dbbc3e26
jenri019 <j.enrique209436@hotmail.com>
2024-02-17 16:08:57 -0600

    Creating loading component

---
+++ src/common/components/IsLoading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const IsLoading = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center loading-component'>
+        <div className="loader"></div>
+    </div>
+  )
+}
