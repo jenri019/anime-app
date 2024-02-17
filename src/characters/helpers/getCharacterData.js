4cdfdf40b792b47dc22a76a9df88ac5583de184b
jenri019 <j.enrique209436@hotmail.com>
2024-02-17 10:50:47 -0600

    Adding API's url to get character info

--- src/characters/helpers/getCharacterData.js
+++ src/characters/helpers/getCharacterData.js
@@ -1,6 +1,15 @@
 export const getCharacterData = async(searchParam = '', type = '') => {
     let url = ``
 
+    switch(type) {
+        case 'ID':
+            url = `https://api.jikan.moe/v4/characters/${searchParam}/full`
+            break;
+        default:
+            url = `https://api.jikan.moe/v4/characters?q=${searchParam}&limit=10`;
+            break;
+    }
+
     const resp = await fetch(url)
     const { data:characters } = await resp.json();
 
