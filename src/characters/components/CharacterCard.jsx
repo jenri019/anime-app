c2f7dbd92db436065e5a9a067c302ea7ea990a36
jenri019 <j.enrique209436@hotmail.com>
2024-02-17 10:49:15 -0600

    Adding content to character card

--- src/characters/components/CharacterCard.jsx
+++ src/characters/components/CharacterCard.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-export const CharacterCard = () => {
+export const CharacterCard = ({
+    images, name = '', mal_id
+}) => {
   return (
-    <div>CharacterCard</div>
+      <div className='col animate__animated animate__fadeInRight'>
+          <div className="card">
+              <div className="row no-gutters">
+                  <div className="col">
+                      <img src={images?.jpg?.image_url } className="card-img" alt={name} />
+                      <div className="card-body text-center">
+                          <Link to={`${mal_id}`} className="card-title">{name}</Link>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   )
 }
