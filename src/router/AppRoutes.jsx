fd55cb3dcc2a3647f7a0e4c3f8a4c40f23f02f1e
jenri019 <j.enrique209436@hotmail.com>
2024-02-19 13:39:38 -0600

    Removing navigation to genres

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -5,7 +5,7 @@ import {
 import { AnimeApp } from "../AnimeApp";
 import { WelcomePage } from "../main/pages/WelcomePage";
 import { AnimeIdPage, MainAnimePage } from "../anime";
-import { MainMangaPage } from "../manga";
+import { MainMangaPage, MangaIdPage } from "../manga";
 import { MainGenresPage } from "../genres";
 import { CharacterIdPage, MainCharacterPage } from "../characters";
 
@@ -39,9 +39,13 @@ export const AppRoutes = createBrowserRouter([
                 element: <MainMangaPage />
             },
             {
+                path: "manga/:id",
+                element: <MangaIdPage />
+            },
+            {/*
                 path: "genres",
                 element: <MainGenresPage />
-            },
+        */},
             {
                 path: "*",
                 element: <Navigate to="/" />
