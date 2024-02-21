edfadce85e71e6d2d2fb5d8535881cfaa3df4f09
jenri019 <j.enrique209436@hotmail.com>
2024-02-20 19:21:08 -0600

    Removing clg

--- src/genres/components/GenresGridContainer.jsx
+++ src/genres/components/GenresGridContainer.jsx
@@ -6,8 +6,6 @@ export const GenresGridContainer = () => {
   const {type, genre, filter} = useContext(GenresContext);
   const {list, status, isLoading} = useFetchListData(type, filter.toString());
 
-  console.log('RES', list.length)
-
   return (
     <div>
       <pre>
