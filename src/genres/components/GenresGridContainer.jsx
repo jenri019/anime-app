a2d8576b55ab7039f83a6f1e7dfb2cf429ea5d71
jenri019 <j.enrique209436@hotmail.com>
2024-02-21 16:58:21 -0600

    Adding status to AnimeByGenre List

--- src/genres/components/GenresGridContainer.jsx
+++ src/genres/components/GenresGridContainer.jsx
@@ -1,16 +1,44 @@
 import React, { useContext } from 'react'
 import { GenresContext } from '../context';
 import { useFetchListData } from '../hooks';
+import { IsLoading, RequestStatus } from '../../common';
+import { AnimeCard } from '../../anime/components';
 
 export const GenresGridContainer = () => {
-  const {type, genre, filter} = useContext(GenresContext);
-  const {list, status, isLoading} = useFetchListData(type, filter.toString());
+  const { type, filter } = useContext(GenresContext);
+  const { list = [], status, isLoading } = useFetchListData(type, filter.toString());
+
+  const PageStatus = () => {
+    return (
+      <>
+        {
+          (list.length === 0 && status === 200 && filter.toString() !== '')
+            ? <RequestStatus size={list.length} status={status} query={filter.toString()} />
+            : <PageInformation />
+        }
+      </>
+    )
+  }
+
+  const PageInformation = () => {
+    return (
+      <div className='anime-list-grid animate__animated animate__fadeInRight mt-2'>
+        {
+          list.map(anime => (
+            <AnimeCard key={anime?.mal_id} {...anime} route='/anime/' />
+          ))
+        }
+      </div>
+    )
+  }
 
   return (
-    <div>
-      <pre>
-        {JSON.stringify(list, null, 5)}
-      </pre>
-    </div>
+    <>
+      {
+        (isLoading)
+          ? <IsLoading />
+          : <PageStatus />
+      }
+    </>
   )
 }
